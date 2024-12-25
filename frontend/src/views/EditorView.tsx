fa8a4895420cd4e90fa5d957bd936c680b624d46
Dan Salmon <dan@salmon.cat>
2024-12-25 15:07:24 -0600

    hover style

--- frontend/src/views/EditorView.tsx
+++ frontend/src/views/EditorView.tsx
@@ -1,6 +1,23 @@
+import {Col, ListGroup, ListGroupItem, Row} from "react-bootstrap";
+import "./EditorView.scss"
+
+function GetEditorItems() {
+    return ["1", "2", "3"]
+}
+
 function EditorView() {
     return (
-        <div>I'm the editor</div>
+        <Row style={{height: "100vh"}}>
+            <Col style={{maxWidth: "15rem"}} className="bg-body-secondary">
+                <ListGroup className="text-start pt-2 list-group-hover">
+                    {GetEditorItems().map(item => (
+                        <ListGroupItem>{item}</ListGroupItem>
+                    ))}
+                </ListGroup>
+            </Col>
+            <Col></Col>
+            <Col></Col>
+        </Row>
     )
 }
 
